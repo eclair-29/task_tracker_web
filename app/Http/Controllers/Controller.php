34fadb66af9e788cf476a0b7077554897c7f2230
Miguel De Chavez <miguel.dechavez.48@gmail.com>
2024-05-08 18:04:35 +0800

    function for redirect when accessing route '/'

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,21 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function index()
+    {
+        $user = auth()->user();
+
+        if ($user) {
+            if ($user->hasRole('user')) {
+                return redirect('/user');
+            }
+
+            if ($user->hasRole('admin')) {
+                return redirect('/backoffice');
+            }
+        }
+
+        return redirect('/login');
+    }
 }
